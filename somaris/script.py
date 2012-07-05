987f976e75454e6d146658c5c91998adb0517de7
Kristoffer Søholm <k.soeholm@gmail.com>
2012-07-05 07:02:40 +0200

    Work work

--- somaris/script.py
+++ somaris/script.py
@@ -7,6 +7,19 @@ f = open(FILE,'r')
 data = f.read()
 data = data.split("\n")
 
-for i in data:
-    if "ReconTaskNumber" in i:
-        print i
+screens = {0 : dict()}
+screenno = 0
+
+for item in data:
+    # Identify series
+    if item.startswith("PROTOCOL_ENTRY_NO:"):
+        screenno = item.split()[1]
+        screens[screenno] = dict()
+        continue
+
+    try:
+        screens[screenno][item.split()[0]] = item.split()[1]
+    except:
+        pass
+
+print screens[0]
