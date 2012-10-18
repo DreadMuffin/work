1cce0179da87f427038072f09ec65f7939a31a98
DreadMuffin <dreadmuffin@gmail.com>
2012-10-18 15:09:51 +0200

    aasdsd

--- somaris/kode/protoscript.py
+++ somaris/kode/protoscript.py
@@ -6,17 +6,17 @@ import os
 FIELDS = "fieldsopdelt"
 
 f = open(FIELDS,'r')
-fields = f.read()
-fields = fields.split("newmode:")
+tfields = f.read()
+tfields = tfields.split("newmode:")
 
-start = fields[0].split("\n")[:-1]
-topogram = fields[1].split("\n")[1:-1]
-ct = fields[2].split("\n")[1:-1]
-pause = fields[3].split("\n")[1:-1]
-pet = fields[4].split("\n")[1:-1]
+start = tfields[0].split("\n")[:-1]
+topogram = tfields[1].split("\n")[1:-1]
+ct = tfields[2].split("\n")[1:-1]
+pause = tfields[3].split("\n")[1:-1]
+pet = tfields[4].split("\n")[1:-1]
 
 
-path = "prototest/"
+path = "protokoller56/"
 path2 = "fprotokoller/"
 listing = os.listdir(path)
 
@@ -26,6 +26,7 @@ def listappend(list,input):
 
 for file in listing:
 #    try:
+
 
         f = open(path + file,'r')
         proto = f.read()
@@ -41,7 +42,9 @@ for file in listing:
         proto = newProto
 
         searching = False
-        fields = start
+        searchingtc = False
+        fields = []
+        listappend(fields,start)
         protoorder = []
     
         for i,item in enumerate(proto):
@@ -49,55 +52,52 @@ for file in listing:
                 if item.startswith("MlPause_Begin"):
                     searching = False
                     listappend(fields,pause)
-                    protoorder.append("p")
+                    protoorder.append("pause")
                 elif item.startswith("MlOtherModalityEntry_Begin"):
                     searching = False
                     listappend(fields,pet)
-                    protoorder.append("r")
-                elif item == "MapProtocolEntryUID: 1":
+                    protoorder.append("pet")
+                elif item.startswith("MlModeEntry_Begin:"):
+                    searchingtc = True
                     searching = False
-                    protoorder.append("t")
+            elif searchingtc:
+                if item == "RangeName[READ_ONLY]: \"Topogram\"":
+                    protoorder.append("topo")
                     listappend(fields,topogram)
-                elif item == "MapProtocolEntryUID: 2":
-                    searching = False
-                    protoorder.append("c")
+                    searchingtc = False
+                elif item.startswith("RangeName"):
+                    protoorder.append("ct")
                     listappend(fields,ct)
+                    searchingtc = False
             elif item.startswith("PROTOCOL_ENTRY_NO"):
                 searching = True
-    
+
+
+        fields.append("MlScanProtocol_End")
+        fields.append("Det her slutter den ikke med")
         findex = 0
         rindex = -1
         recon = [0] * (len(protoorder) - protoorder.count("p"))
+        protonr = 0
         fieldlist = ""
         reconpos = [i for i, x in enumerate(fields) if x == "No_Of_Valid_Recons"]
-      
-
-        print fields
     
         for i,item in enumerate(proto):
             if item.startswith(fields[findex]):
                 if item.startswith("No_Of_Valid_Recons"):
                     rindex+=1
                     recon[rindex] = int(item.split(" ")[1])
-                    print recon
                 elif item.startswith("MlModeRecon_End"):
                     recon[rindex]-= 1
                     if recon[rindex] > 0:
                         findex = reconpos[rindex]
-#                        if rindex == 0:
-#                            findex = reconpos[0]
-#                        elif rindex == 1:
-#                            findex = reconpos[1]
-#                        else: findex = reconpos[2]
+                elif item.startswith("PROTOCOL_ENTRY_NO"):
+                    item = protoorder[protonr] + "\n" + item
+                    protonr+=1
                 findex+=1
-                fieldlist+=item + " " + str(i) + "\n"
-#            if item.startswith("MapProtocolEntryUID:") and int(item[-1]) == 6:
-#                print file + " " + item
+                fieldlist+=item + "\n"
+                
     
-
-
-
-
         fieldlist = fieldlist[:-1]
         f = open(path2 + "fields56_" + file,'w')
         f.write(fieldlist)
